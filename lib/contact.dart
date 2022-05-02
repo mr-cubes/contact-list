211c615168bac537515cae51147236e95139465f
neilcantorne <cantorne.neil@gmail.com>
2022-05-03 04:36:41 +0800

    implement contact class

---
+++ lib/contact.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Contact {
+  final String name;
+  final String mobile;
+  final String email;
+  final Image? avatar;
+
+  Contact(this.name, this.mobile, this.email, this.avatar);
+}
