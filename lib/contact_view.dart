7337735b2f72ee4f6855609dbd49bb35c10e0bb4
neilcantorne <cantorne.neil@gmail.com>
2022-05-10 06:41:33 +0800

    implement contact view

--- lib/contact_view.dart
+++ lib/contact_view.dart
@@ -11,6 +11,49 @@ class ContactView extends StatelessWidget {
       appBar: AppBar(
         title: Text("${contact.name} profile"),
       ),
+      body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 400),
+              child: Column(children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Expanded(
+                      child: CircleAvatar(
+                    minRadius: 80,
+                    maxRadius: 110,
+                    foregroundImage:
+                        (contact.avatar == null) ? null : contact.avatar!.image,
+                  )),
+                ),
+                const SizedBox(height: 25),
+                Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Text(
+                      contact.name,
+                      style: const TextStyle(fontSize: 19),
+                    )),
+                Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 6),
+                    child: Text("Email: ${contact.email}")),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  child: Text("Mobile number: ${contact.mobile}"),
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: SizedBox(
+                      height: 40,
+                      width: 120,
+                      child: ElevatedButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: const Text("Back"))),
+                )
+              ]),
+            ),
+          )),
     );
   }
 }
