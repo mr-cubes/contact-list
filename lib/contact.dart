2b9fb0ba70364d492aaadb071d9e8bdd2ac0c2b8
neilcantorne <cantorne.neil@gmail.com>
2022-05-11 07:08:49 +0800

    implement fromJson

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class Contact {
@@ -7,4 +9,12 @@ class Contact {
   final Image? avatar;
 
   Contact(this.name, this.mobile, this.email, this.avatar);
+
+  static Contact fromJson(dynamic json) => Contact(
+      json["name"],
+      json["mobile"],
+      json["email"],
+      json["avatar"] == null
+          ? null
+          : Image.memory(base64Decode(json["avatar"])));
 }
