e50f8ead10ca2132f1bb9cf0ccc22471abdf004f
neilcantorne <cantorne.neil@gmail.com>
2022-05-12 07:33:31 +0800

    implement data loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
+import 'package:contact_list/contact.dart';
 import 'package:contact_list/list_page.dart';
 import 'package:flutter/material.dart';
+import 'package:localstorage/localstorage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   static final ValueNotifier<ThemeMode> themeNotifier =
       ValueNotifier(ThemeMode.light);
-  const MyApp({super.key});
+  final LocalStorage storage = LocalStorage('data');
 
+  MyApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<ThemeMode>(
         valueListenable: themeNotifier,
@@ -20,8 +25,23 @@ class MyApp extends StatelessWidget {
             theme: ThemeData(primarySwatch: Colors.indigo),
             darkTheme: ThemeData.dark(),
             themeMode: currentMode,
-            home: const ListPage(),
+            home: FutureBuilder(future: storage.ready, builder: futureBuild),
           );
         });
+  }
+
+  Widget futureBuild(BuildContext context, AsyncSnapshot<bool> snapshot) {
+    if (snapshot.data == true) {
+      List<dynamic>? data = storage.getItem('contacts');
+
+      if (data == null) {
+        storage.setItem('contacts', []).then((value) => {});
+        data = [];
+      }
+
+      return ListPage(
+          contacts: List.from(data.map((json) => Contact.fromJson(json))));
+    }
+    return const CircularProgressIndicator.adaptive();
   }
 }
